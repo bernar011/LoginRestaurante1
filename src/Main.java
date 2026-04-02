6b78e1f17dbb2fd3a70b2b0532a2ffe6487b1747
nelson <nelsonferraz1996@gmail.com>
2026-04-02 16:56:35 +0100

    Adicionar o !

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Login realizado com sucesso!");
 
-        String senha = "Admin@123";
+        String senha = "Admin@123!";
     }
 }
